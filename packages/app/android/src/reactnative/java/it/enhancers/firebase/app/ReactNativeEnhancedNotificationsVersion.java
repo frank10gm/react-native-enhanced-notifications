3cfeae81af801a24e5a903f249d1527f3c477ae7
Francesco La Placa <frank10gm@gmail.com>
2022-07-19 15:31:46 +0200

    bump version

--- packages/app/android/src/reactnative/java/it/enhancers/firebase/app/ReactNativeEnhancedNotificationsVersion.java
+++ packages/app/android/src/reactnative/java/it/enhancers/firebase/app/ReactNativeEnhancedNotificationsVersion.java
@@ -18,5 +18,5 @@ package it.enhancers.firebase.app;
  */
 // generated file - do not modify or commit
 public class ReactNativeEnhancedNotificationsVersion {
-  public static String VERSION = "20.0.1";
+  public static String VERSION = "20.0.2";
 }
